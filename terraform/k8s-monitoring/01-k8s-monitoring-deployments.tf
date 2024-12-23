e7914dad1c7cd8621764c7b3d09eac74ca19d80b
micheledellipaoli-pagopa <michele.dellipaoli@pagopa.it>
2024-12-23 14:40:50 +0100

    add tags to module and update tag

--- terraform/k8s-monitoring/01-k8s-monitoring-deployments.tf
+++ terraform/k8s-monitoring/01-k8s-monitoring-deployments.tf
@@ -18,7 +18,7 @@ locals {
 module "k8s_deployment_monitoring" {
   for_each = toset(local.microservices_names)
 
-  source = "git::https://github.com/pagopa/interop-infra-commons//terraform/modules/k8s-deployment-monitoring?ref=v1.3.5"
+  source = "git::https://github.com/pagopa/interop-infra-commons//terraform/modules/k8s-deployment-monitoring?ref=v1.4.5"
 
   env                 = var.env
   eks_cluster_name    = var.eks_cluster_name
@@ -39,4 +39,6 @@ module "k8s_deployment_monitoring" {
 
   cloudwatch_app_logs_errors_metric_name      = try(data.external.cloudwatch_log_metric_filters.result.metricName, null)
   cloudwatch_app_logs_errors_metric_namespace = try(data.external.cloudwatch_log_metric_filters.result.metricNamespace, null)
+
+  tags = var.tags
 }
