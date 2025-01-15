24dd3b2fd952df0053044ba6e0aa35bfb0fec1e2
micheledellipaoli-pagopa <michele.dellipaoli@pagopa.it>
2025-01-15 16:00:30 +0100

    remove comments and edit tf-apply-secrets workflow to perform apply instead of plan

--- terraform/k8s-secrets/10-secrets.tf
+++ terraform/k8s-secrets/10-secrets.tf
@@ -27,29 +27,17 @@ data "kubernetes_namespace_v1" "env" {
   }
 }
 
-# output "print_secrets_name" {
-#   value = data.aws_secretsmanager_secret.tracing_store_users_object
-# }
-
-# output "print_secrets_version" {
-#   value = keys(data.aws_secretsmanager_secret_version.tracing_store_users)
-# }
-
 locals {
   sv_namespaces_pairs = flatten([
     for sv_key, sv_value in data.aws_secretsmanager_secret_version.tracing_store_users : [
       for ns in toset(split(" ", (data.aws_secretsmanager_secret.tracing_store_users_object[sv_key].tags["EKSClusterNamespacesSpaceSeparated"]))) : {
         eks_replica_secret_name = data.aws_secretsmanager_secret.tracing_store_users_object[sv_value.secret_id].tags["EKSReplicaSecretName"],
-        secret_version          = sv_value, #EDIT to sv_key to print the related output
+        secret_version          = sv_value,
         namespace               = ns
       }
     ]
   ])
 }
-
-# output "print_sv_namespaces_pairs_local" {
-#   value = local.sv_namespaces_pairs
-# }
 
 resource "kubernetes_secret_v1" "tracing_store_users" {
   for_each = { for elem in local.sv_namespaces_pairs : "${elem.eks_replica_secret_name}/${elem.namespace}" => elem }
